d74eb091e0d8d33365cf050349712edf0987e165
GitLabUser01 <oovaChzETP7YOfGAovOUOE5IG>
2018-11-19 14:31:19 +0000

    work on db

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from config import (API_KEY, API_SECRET)
+from src.config import (API_KEY, API_SECRET)
 from binance.client import Client
-client = Client(API_KEY, API_SECRET)
-depth = client.get_order_book(symbol='BTCUSDT')
+
+
+def create_db():
+    print('hi')
 
 
 def main():
---
+++ src/mysql_scripts.sql
@@ -0,0 +1,8 @@
+SHOW DATABASES;
+
+DROP DATABASE IF EXISTS capstone;
+CREATE DATABASE capstone;
+
+CREATE OR REPLACE TABLE table1 (a int);
+
+select * from table1
